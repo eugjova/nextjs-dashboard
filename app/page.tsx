633407c323f1dbe4f0a092616004379d68f81fc4
eugjova <eugeniejovanca@gmail.com>
2024-02-22 18:22:36 +0700

    add console.log

--- app/page.tsx
+++ app/page.tsx
@@ -29,6 +29,7 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          {/* console.log("Hello World")}
         </div>
       </div>
     </main>

ccc4c0b08de4a4fd49a8be85344099fb939d4402
eugjova <eugeniejovanca@gmail.com>
2024-11-17 00:37:54 +0700

    fix gitignore

--- app/Api/route.ts
+++ app/Api/route.ts
@@ -1,28 +1,28 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { getSession } from 'next-auth/react'
+// import { NextApiRequest, NextApiResponse } from 'next'
+// import { getSession } from 'next-auth/react'
  
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const session = await getSession()
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const session = await getSession()
  
-  // Check if the user is authenticated
-  if (!session) {
-    res.status(401).json({
-      error: 'User is not authenticated',
-    })
-    return
-  }
+//   // Check if the user is authenticated
+//   if (!session) {
+//     res.status(401).json({
+//       error: 'User is not authenticated',
+//     })
+//     return
+//   }
  
-  // Check if the user has the 'admin' role
-  if (session.user?.id!== 'owner') {
-    res.status(401).json({
-      error: 'Unauthorized access: User does not have admin privileges.',
-    })
-    return
-  }
+//   // Check if the user has the 'admin' role
+//   if (session.user?.id!== 'owner') {
+//     res.status(401).json({
+//       error: 'Unauthorized access: User does not have admin privileges.',
+//     })
+//     return
+//   }
  
-  // Proceed with the route for authorized users
-  // ... implementation of the API Route
-}
+//   // Proceed with the route for authorized users
+//   // ... implementation of the API Route
+// }

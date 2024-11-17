2fc52196515e78018f946b569b494df9b35c534a
eugjova <eugeniejovanca@gmail.com>
2024-11-17 22:30:09 +0700

    Revert "fix null parameter on authenticate"
    This reverts commit fcf3a9142bc8d1ddc95a4858540aa8a7b21b69ab.

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -8,53 +8,30 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightCircleIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
-import { useState } from 'react';
+import { useFormState, useFormStatus } from 'react-dom';
 import { authenticate } from '../lib/action';
 
 export default function LoginForm() {
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
-  const [pending, setPending] = useState(false);
-
-  async function dispatch(formData: FormData) {
-    try {
-      setPending(true);
-      const errorMessage = await authenticate(undefined, formData);
-      if (errorMessage) {
-        setErrorMessage(errorMessage); // Set error message jika ada
-      } else {
-        setErrorMessage(null); // Reset error message jika berhasil
-      }
-    } catch (error: any) {
-      setErrorMessage('an unexpected error ocurred.');
-    } finally {
-      setPending(false);
-    }
-  }
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
 
   return (
-    <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          const formData = new FormData(event.currentTarget);
-          dispatch(formData);
-        }}
-        className="space-y-3"
-      >
+    <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-purple-700 bg-opacity-0 px-6 pb-4 pt-8">
-        <h1 className={`${oswald.variable} mb-3 text-xl`}>
+
+        <h1 className={`${oswald.className} mb-3 text-xl`}>
           Please log in to continue.
         </h1>
         <div className="w-full">
           <div>
             <label
-              className={`${oswald.variable} mb-3 mt-5 block text-xs font-medium text-black`}
+              className={`${oswald.className} mb-3 mt-5 block text-xs font-medium text-black`}
               htmlFor="email"
             >
               Email
             </label>
             <div className="relative">
               <input
-                className={`${roboto.variable} peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-black text-sm outline-2 placeholder:text-gray-500`}
+                className={`${roboto.className} peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-black text-sm outline-2 placeholder:text-gray-500`}
                 id="email"
                 type="email"
                 name="email"
@@ -66,14 +43,14 @@ export default function LoginForm() {
           </div>
           <div className="mt-4">
             <label
-              className={`${oswald.variable} mb-3 mt-5 block text-xs font-medium text-black`}
+              className={`${oswald.className} mb-3 mt-5 block text-xs font-medium text-black`}
               htmlFor="password"
             >
               Password
             </label>
             <div className="relative">
               <input
-                className={`${roboto.variable} peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-black text-sm outline-2 placeholder:text-gray-500`}
+                className={`${roboto.className} peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-black text-sm outline-2 placeholder:text-gray-500`}
                 id="password"
                 type="password"
                 name="password"
@@ -85,7 +62,7 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <LoginButton pending={pending} />
+        <LoginButton />
         <div
           className="flex h-8 items-end space-x-1"
           aria-live="polite"
@@ -103,7 +80,9 @@ export default function LoginForm() {
   );
 }
 
-function LoginButton({ pending }: { pending: boolean }) {
+function LoginButton() {
+  const { pending } = useFormStatus();
+
   return (
     <Button className="mt-4 w-full" aria-disabled={pending}>
       Log in <ArrowRightCircleIcon className="ml-auto h-5 w-5 text-gray-50" />
